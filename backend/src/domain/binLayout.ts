7158e2fb44e2aa2a272067b7432bfae43d0e650d
donaldsbrain <donald.vanraalte@gmail.com>
2024-10-08 10:21:49 -0500

    basic domain

--- backend/src/domain/binLayout.ts
+++ backend/src/domain/binLayout.ts
@@ -1,3 +1,35 @@
+import { Dimension, Point } from "hmi/shapePrimitives";
+import { GraphicPrimitive } from "hmi/graphicPrimitives";
+import { v4 as uuidv4 } from 'uuid'
+
+export type Bin = RectangularBin | CircularBin
+
 export type BinLayout = {
-    dimensionX: number;
+    id: string
+    dimensions: Dimension
+    lineThickness: number
+    bins: Bin[]
+    graphics: GraphicPrimitive[]
+}
+
+export function binLayout(args: { id?: string, dimensions: Dimension, lineThickness?: number }): BinLayout {
+    return {
+        id: args.id ?? uuidv4(),
+        dimensions: args.dimensions,
+        lineThickness: args.lineThickness ?? 1,
+        bins: [],
+        graphics: []
+    }
+}
+
+export type RectangularBin = {
+    type: 'RectangularBin'
+    center: Point
+    dimensions: Dimension
+}
+
+export type CircularBin = {
+    type: 'CircularBin'
+    center: Point
+    radius: number
 }
